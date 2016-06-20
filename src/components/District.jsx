1dcfc7a794bb8e8f535e5b548e684317f1d32cb3
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-19 18:43:51 -1000

    added political party

--- src/components/District.jsx
+++ src/components/District.jsx
@@ -11,7 +11,7 @@ export default React.createClass({
   render: function() {
     const mailto = "mailto:" + this.getDistrictInfo().contact_email;
     const districtInfo = this.getDistrictInfo();
-    const politicalParty = districtInfo.politician_party;
+    const politicalParty = districtInfo.politician_party.slice(0,1) + '.';
 
     return <div className="district">
       <h4>District Information</h4>
@@ -19,7 +19,7 @@ export default React.createClass({
         <i><p>Now previewing {districtInfo.politician_officetype} District {districtInfo.district_number}</p></i>
         <img className="thumbnail" id="photo" src={districtInfo.politician_picture} height="151" width="121" />
       </div>
-      <p>{districtInfo.politician_position} {districtInfo.politician_firstname} {districtInfo.politician_lastname}</p>
+      <p>{districtInfo.politician_position} {districtInfo.politician_firstname} {districtInfo.politician_lastname} ({politicalParty})</p>
       <p>E-mail: <a href={mailto}>{districtInfo.contact_email}</a></p>
       <p>Tel: {districtInfo.contact_phone}</p>
     </div>;
