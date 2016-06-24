21e42ac879c823073da66a9498f58f4b90571c87
kwhitejr <kwhitejr.edu@gmail.com>
2016-06-23 18:39:49 -1000

    trying to serve index

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
 
 app.route('/')
   .get(function (req, res) {
-    res.render('index');
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 
 app.route('/district')
